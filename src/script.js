105b05310f24c39d301a95afbb72cbb2352178b9
Matty <matteomania05@gmail.com>
2022-11-04 19:35:40 +0100

    Aggiunta (prima)

---
+++ src/script.js
@@ -0,0 +1,9 @@
+function nav() {
+	document.querySelector(".links").classList.toggle("shown");
+	document.querySelector(".container").classList.toggle("full");
+	/*document.querySelector(".line1").classList.toggle("changed1");
+	document.querySelector(".line2").classList.toggle("changed2");
+	document.querySelector(".line3").classList.toggle("changed3");*/
+    document.querySelector(".hamburger").classList.toggle("open");
+}
+
