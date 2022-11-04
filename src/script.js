c36890affd19a9825f3189b8392920ae6e19240e
Matty <matteomania05@gmail.com>
2022-11-04 23:55:12 +0100

    Fix IOS autoplay bug

--- src/script.js
+++ src/script.js
@@ -6,3 +6,32 @@ function nav() {
 	document.querySelector(".line3").classList.toggle("changed3");*/
     document.querySelector(".hamburger").classList.toggle("open");
 }
+
+
+/**
+ * video check
+ */
+var video = document.querySelector("#video");
+
+function startPlayback() {
+	if (!video) {
+		video = document.createElement('video');
+		video.src = './public/video/impennata2.MP4';
+		video.loop = true;
+		video.addEventListener('playing', paintVideo);
+		console.log("not work");
+	}
+	else if (video == null || video == ErrorEvent) {
+		/* second check */
+		var bgvideo = document.getElementById("video");
+		bgvideo.muted = true;
+		bgvideo.loop = true;
+		bgvideo.playsinline = true;
+		bgvideo.play();
+	}
+	else {
+		//nothing, it works.
+		console.log("works.")
+	}
+	video.play();
+}
