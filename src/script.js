ba1bdabefb832e0f9682bd7317e8fd997165ba91
Matteo0099 <matteomania05@gmail.com>
2022-12-07 19:12:56 +0100

    Some fix

--- src/script.js
+++ src/script.js
@@ -1,12 +1,46 @@
 function nav() {
 	document.querySelector(".links").classList.toggle("shown");
 	document.querySelector(".container").classList.toggle("full");
+	/**change if only y use personalized animation */
 	/*document.querySelector(".line1").classList.toggle("changed1");
 	document.querySelector(".line2").classList.toggle("changed2");
 	document.querySelector(".line3").classList.toggle("changed3");*/
-    document.querySelector(".hamburger").classList.toggle("open");
+	document.querySelector(".hamburger").classList.toggle("open");
 }
 
+/**back to top 
+ * button
+ * with jquery!
+ */
+$(document).ready(function () {
+	$(window).scroll(function () {
+		//mobile scroll and desktop
+		if ($(this).scrollTop() > 767) {
+			$('a.turn__top').fadeIn();
+		} else {
+			$('a.turn__top').fadeOut();
+		}
+	});
+	$('.scrollup').click(function () {
+		$("html, body").animate({
+			scrollTop: 0
+		}, 600);
+		return false;
+	});
+});
+
+// let width = window.innerWidth;
+// let Height = window.innerHeight;
+// console.log(Height);
+
+// function TurnTop(e, style) {
+// 	var item = document.querySelector('.turn__top');
+// 	if (Height > 750) {
+// 		item.style["display"] = "none";
+// 	} else {
+// 		item.style["display"] = "block";
+// 	} 
+// }
 
 /**
  * video check
