e2f1c0e6c59bffc766cc319f0aa6517027577704
Matteo Manià <matteomania05@gmail.com>
2023-12-26 17:14:23 +0100

    some fix & adds

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,68 @@
 </script>
 
 <template>
-  <div id="app">
+  <div id="app" class="overflow-x-hidden">
     <!-- main page -->
     <main class="main-content min-h-screen">
-      <!-- navigation -->
-      <div class="w-full mx-auto">
-        <nav class="flex justify-between items-center pt-4 w-full mx-auto">
-          <div class="w-full ml-1 sm:ml-6">
-            <RouterLink to="/" class="font-mono text-2xl" alt="logo">
-              <img src="/logo.jpeg" alt="Logo" title="XJXVISUALS" role="img" aria-label="image" class=" max-w-20 block" />
+      <video autoplay muted loop id="myVideo">
+        <source src="https://player.vimeo.com/progressive_redirect/playback/897102984/rendition/1080p/file.mp4?loc=external&log_user=0&signature=9328f48ed5eac8a60ced387f8095a5aa71e185466ceb23e06603d5ff2e61577a" type="video/mp4">
+        Your browser does not support HTML5 video.
+      </video> 
+      <!-- (fake) navigation -->
+      <div class="w-full sticky top-0 left-0 right-0 z-[999] max-h-20 min-h-[40px] h-full bg-accent border-2 border-accent outline-2 outline-accent p-0">
+        <nav class="fakeNav py-1.5 flex justify-start items-center">
+          <div class="social-wrap flex ml-5 gap-3 text-sm">
+            <a href="" target="_blank">
+              <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler hover:opacity-70 icon-tabler-brand-instagram" width="18"
+                height="18" viewBox="0 0 24 24" stroke-width="1.9" stroke="#fefefe" fill="none" stroke-linecap="round"
+                stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z" />
+                <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" fill="#fefefe" />
+                <path d="M16.5 7.5l0 .01" fill="#fefefe" />
+              </svg>
+            </a>
+            <a href="" target="_blank">
+              <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler hover:opacity-70 icon-tabler-brand-facebook" width="18"
+                height="18" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fefefe" fill="none" stroke-linecap="round"
+                stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" fill="#fefefe" />
+              </svg>
+            </a>
+            <a href="" target="_blank">
+              <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler hover:opacity-70 icon-tabler-brand-youtube-filled" width="18"
+                height="18" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fefefe" fill="none" stroke-linecap="round"
+                stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path
+                  d="M18 3a5 5 0 0 1 5 5v8a5 5 0 0 1 -5 5h-12a5 5 0 0 1 -5 -5v-8a5 5 0 0 1 5 -5zm-9 6v6a1 1 0 0 0 1.514 .857l5 -3a1 1 0 0 0 0 -1.714l-5 -3a1 1 0 0 0 -1.514 .857z"
+                  stroke-width="0" fill="#fefefe" />
+              </svg>
+            </a>
+            <a href="" target="_blank">
+              <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler hover:opacity-70 icon-tabler-brand-tiktok-filled" width="18"
+                height="18" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fefefe" fill="none" stroke-linecap="round"
+                stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path
+                  d="M16.083 2h-4.083a1 1 0 0 0 -1 1v11.5a1.5 1.5 0 1 1 -2.519 -1.1l.12 -.1a1 1 0 0 0 .399 -.8v-4.326a1 1 0 0 0 -1.23 -.974a7.5 7.5 0 0 0 1.73 14.8l.243 -.005a7.5 7.5 0 0 0 7.257 -7.495v-2.7l.311 .153c1.122 .53 2.333 .868 3.59 .993a1 1 0 0 0 1.099 -.996v-4.033a1 1 0 0 0 -.834 -.986a5.005 5.005 0 0 1 -4.097 -4.096a1 1 0 0 0 -.986 -.835z"
+                  stroke-width="0" fill="#fefefe" />
+              </svg>
+            </a>
+          </div>
+          <div class="message w-[80%] text-center">
+            <h1>PLEASE NOTE: i'm not avaible from 23 december to 7 january!</h1>
+          </div>
+        </nav>
+      </div>
+      <!-- TRUE NAV -->
+      <div class="w-full mx-auto sticky z-[999]">
+        <nav class="flex justify-between z-[999] items-center pt-4 w-full mx-auto">
+          <div class="ml-1 sm:ml-6">
+            <RouterLink to="/" class="flex justify-center items-center gap-4 font-mono text-2xl" alt="logo">
+              <img src="/logo.jpeg" alt="Logo" title="XJXVISUALS" role="img" aria-label="image" class=" max-w-10 block" />
+              <span class="font-black m-0 italic text-3xl uppercase">XJXSTUDIOS</span>
             </RouterLink>
           </div>
           <div class="hamburger cursor-pointer mr-1 sm:mr-6" @click="openNav">
@@ -23,7 +76,7 @@
             </svg>
           </div>
         </nav>
-      </div>
+      </div>     
       <!-- for content of the page -->
       <router-view v-slot="{ Component, route }">
         <div :key="route.name" class="min-h-screen max-w-full overflow-x-hidden">
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,62 @@
 <template>
-  <div class="container mt-10 flex justify-center mx-auto">
-    <h1 class="text-2xl md:text-3xl">Home page</h1>
+  <div class="container flex mt-6 justify-center flex-col mx-auto">
+    <div class="content max-w-4xl mx-auto">
+      <h1 class="font-black text-2xl sm:text-3xl mb-3 uppercase">Last Video</h1>
+      <p class="font-regular text-lg max-w-3xl my-4">
+        "STREET DRIFTING BMW E30 SWAPPED"
+      </p>
+      <button id="myBtn" class="flex justify-center items-center gap-2 text-sm mt-6 py-3 px-4">
+        VIEW VIDEO
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-right-bar" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fefefe" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+          <path d="M18 15l3 -3l-3 -3" />
+          <path d="M3 12h18" />
+          <path d="M3 9v6" />
+        </svg>
+      </button>
+    </div>
+    <div class="scroll imgBox" style="--time:100s">
+      <div>
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+      </div>
+      <div>
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+        <img src="/logo.jpeg" role="img" title="logo XJXSTUDIS" alt="Image">
+      </div>
+    </div>
   </div>
 </template>
 
