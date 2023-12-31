92235c305835728d545b1bea0bc1cc4ea8131772
Matty <74145047+Matteo0099@users.noreply.github.com>
2023-12-31 22:17:08 +0100

    fixing layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,60 +1,108 @@
 <template>
-  <div class="container w-full h-screen flex mt-6 justify-center flex-col mx-auto">
-    <div class="content max-w-4xl mx-auto">
-      <h1 class="font-black text-2xl sm:text-3xl mb-3 uppercase">Last Video</h1>
-      <p class="font-regular text-lg max-w-3xl my-4">
-        "STREET DRIFTING BMW E30 SWAPPED"
-      </p>
-      <button id="myBtn" class="flex justify-center items-center gap-2 text-sm mt-6 py-3 px-4">
-        VIEW VIDEO
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-right-bar" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fefefe" fill="none" stroke-linecap="round" stroke-linejoin="round">
-          <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-          <path d="M18 15l3 -3l-3 -3" />
-          <path d="M3 12h18" />
-          <path d="M3 9v6" />
-        </svg>
-      </button>
-    </div>
-    <div class="scroll imgBox" style="--time:100s">
-      <div>
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+  <div class="min-h-screen w-full flex flex-col">
+    <div class="container w-full h-full min-h-screen flex mt-6 justify-center flex-col mx-auto">
+      <div class="min-h-screen h-full">
+        <div class="content max-w-4xl mx-auto">
+          <h1 class="font-black text-2xl sm:text-3xl mb-3 uppercase">Last Video</h1>
+          <p class="font-regular text-lg max-w-3xl my-4">
+            "STREET DRIFTING BMW E30 SWAPPED"
+          </p>
+          <button id="myBtn" class="flex justify-center items-center gap-2 text-sm mt-6 py-3 px-4">
+            VIEW VIDEO
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-right-bar" width="28"
+              height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fefefe" fill="none" stroke-linecap="round"
+              stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M18 15l3 -3l-3 -3" />
+              <path d="M3 12h18" />
+              <path d="M3 9v6" />
+            </svg>
+          </button>
+        </div>
+        <!-- scroller horizontal -->
+        <div class="scroll imgBox" style="--time:100s">
+          <div>
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+          </div>
+          <div>
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+            <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+          </div>
+        </div>
       </div>
-      <div>
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
-        <img src="/logo_2.png" role="img" title="logo XJXSTUDIS" alt="Image">
+      <!-- content of all the home -->
+      <div class="container sticky w-full max-w-4xl mx-auto flex flex-col justify-center items-center">
+        <div class="relative min-h-screen mx-4 w-full">
+          <h1 class="text-neutral-100 text-2xl sm:text-3xl font-black">
+            PREVIEW
+          </h1>
+          <!-- 1 -->
+          <div class="card h-96 gap-2 flex flex-wrap sm:flex-nowrap w-full">
+            <div class="px-4 py-2 w-full border-2 border-neutral-100 my-2 bg-neutral-900">
+              <h1>Title 1</h1>
+            </div>
+            <div class="px-4 py-2 w-full border-2 border-neutral-100 my-2 bg-neutral-900">
+              <h1>Title 2</h1>
+            </div>
+          </div>
+          <!-- 2 -->
+          <div class="card h-96 gap-2 flex flex-wrap sm:flex-nowrap w-full">
+            <div class="px-4 py-2 w-full border-2 border-neutral-100 my-2 bg-neutral-900">
+              <h1>Title 3</h1>
+            </div>
+            <div class="px-4 py-2 w-full border-2 border-neutral-100 my-2 bg-neutral-900">
+              <h1>Title 4</h1>
+            </div>
+          </div>
+
+          <!-- about me -->
+          <h1 class="mt-20 text-neutral-100 text-2xl sm:text-3xl font-black">ABOUT ME</h1>
+          <div class="about text-neutral-50 w-full flex flex-wrap sm:flex-nowrap">
+            <div class="flex w-1/2">
+              <img src="" width="400" height="300" alt="Image Profile" role="img">
+            </div>
+            <div class="flex w-1/2">
+              <h1>Francesco Versace</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur commodi nulla dolores cum, error possimus eum excepturi quis earum a. Id ipsum labore repellendus maxime nesciunt placeat pariatur neque sint.
+              Excepturi expedita esse dolores, molestiae nisi quisquam aperiam voluptates molestias deleniti natus accusantium aut. Fugiat repellendus corporis atque exercitationem asperiores, quas excepturi ipsa obcaecati quam illum omnis esse, accusamus ipsam!
+              Quisquam reiciendis est, optio velit ab eaque maxime ipsam temporibus aspernatur quam nam blanditiis exercitationem facilis a, cupiditate, nemo sunt maiores doloremque corrupti enim non! Beatae illum quo ad est?
+              Molestiae esse libero optio! Exercitationem amet aliquid, fugiat odit obcaecati hic itaque non eveniet ea consequuntur modi quaerat. Fuga explicabo, amet exercitationem eligendi vel ipsum impedit sint quibusdam ut doloremque.</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
