87cf3fff5539de6bfee8f7301477131e8982bb95
Matteo Manià <matteomania05@gmail.com>
2024-01-03 13:31:39 +0100

    some update (home.vue)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
         <div class="content max-w-4xl mx-auto">
           <h1 class="font-black text-2xl sm:text-3xl mb-3 uppercase">Last Video</h1>
           <p class="font-regular text-lg max-w-3xl my-4">
-            "STREET DRIFTING BMW E30 SWAPPED"
+            "Checkout last video of the Scarbmafia's raduno"
           </p>
           <button id="myBtn" class="flex justify-center items-center gap-2 text-sm mt-6 py-3 px-4">
             VIEW VIDEO
@@ -64,8 +64,9 @@
         </div>
       </div>
       <!-- content of all the home -->
-      <div class="container sticky w-full max-w-4xl mx-auto flex flex-col justify-center items-center">
-        <div class="relative min-h-screen mx-4 w-full">
+      <div class="container sticky w-full max-w-4xl px-4 mt-24 mx-auto flex flex-col justify-center items-center">
+        <div class="relative min-h-screen w-full">
+          <!-- LITTLE PREVIEW OF WORKS -->
           <h1 class="text-neutral-100 text-2xl sm:text-3xl font-black">
             PREVIEW
           </h1>
@@ -89,17 +90,61 @@
           </div>
 
           <!-- about me -->
-          <h1 class="mt-20 text-neutral-100 text-2xl sm:text-3xl font-black">ABOUT ME</h1>
-          <div class="about text-neutral-50 w-full flex flex-wrap sm:flex-nowrap">
-            <div class="flex w-1/2">
-              <img src="" width="400" height="300" alt="Image Profile" role="img">
+          <h1 class="mt-48 text-neutral-100 text-2xl sm:text-3xl font-black">ABOUT ME</h1>
+          <div class="about text-neutral-50 w-full mt-4 flex flex-wrap sm:flex-nowrap">
+            <div class="flex w-1/2 flex-col justify-center items-start">
+              <img src="/logo_2.png" width="150" height="150" alt="Image Profile" role="img">
             </div>
-            <div class="flex w-1/2">
-              <h1>Francesco Versace</h1>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur commodi nulla dolores cum, error possimus eum excepturi quis earum a. Id ipsum labore repellendus maxime nesciunt placeat pariatur neque sint.
-              Excepturi expedita esse dolores, molestiae nisi quisquam aperiam voluptates molestias deleniti natus accusantium aut. Fugiat repellendus corporis atque exercitationem asperiores, quas excepturi ipsa obcaecati quam illum omnis esse, accusamus ipsam!
-              Quisquam reiciendis est, optio velit ab eaque maxime ipsam temporibus aspernatur quam nam blanditiis exercitationem facilis a, cupiditate, nemo sunt maiores doloremque corrupti enim non! Beatae illum quo ad est?
-              Molestiae esse libero optio! Exercitationem amet aliquid, fugiat odit obcaecati hic itaque non eveniet ea consequuntur modi quaerat. Fuga explicabo, amet exercitationem eligendi vel ipsum impedit sint quibusdam ut doloremque.</p>
+            <div class="flex w-1/2 flex-col justify-center items-start">
+              <h1 class="font-bold text-2xl sm:text-3xl mb-4">Francesco Versace</h1>
+              <p>I'm 19 y.o. video-maker that put passion and love in whatever i do!</p>
+              <p>Feel free to contact me for a video service.</p>
+            </div>
+          </div>
+
+          <!-- SERVICES -->
+          <div class="container mx-auto mt-48 mb-24">
+            <h1 class="text-neutral-100 text-2xl sm:text-3xl font-black">MY SERVICES</h1>
+            <div class="flex w-full flex-col mx-auto sm:mx-0 sm:flex-row sm:justify-between flex-wrap sm:flex-nowrap mt-8">
+              <div class="w-full sm:w-1/3 px-4 py-3 border-2 border-neutral-200 flex flex-col justify-center items-start">
+                <h1>Base</h1>
+                <p>200$</p>
+
+                <ul>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                </ul>
+              </div>
+              <div class="w-full sm:w-1/3 px-4 py-3 sm:-translate-y-4 border-2 sm:mx-2 border-neutral-200 flex flex-col justify-center items-start">
+                <h1>Interprise</h1>
+                <p>350$</p>
+
+                <ul>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                </ul>
+              </div>
+              <div class="w-full sm:w-1/3 px-4 py-3 border-2 border-neutral-200 flex flex-col justify-center items-start">
+                <h1>Premium</h1>
+                <p>550$</p>
+
+                <ul>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
